8b55bec31fc21996f6d010996852963f06e43c23
Cleclemi28 <c.trofleau@gmail.com>
2017-03-06 11:44:54 +0100

    commit des requete xml

--- studentsList.js
+++ studentsList.js
@@ -1,1 +1,16 @@
-// Write yout Javascript code in this files
+var request = new XMLHttpRequest();
+
+request.open('GET', 'student.json', true);
+
+request.onreadystatechange = function(aEvt) {
+    if (request.readyState == 4) {
+        if (request.status == 200){
+					var students = JSON.parse(request,responseText);
+					console.log(students)
+				}else{
+					  dump("Erreur pendant le chargement de la page.\n");
+				}
+
+    }
+};
+request.send();
